3e5cec6214426b227e480f67b4405be56f6df798
Vamsikirankallem <kallemvamsikiran@gmail.com>
2025-10-17 09:30:56 +0530

    jwt token time updated

--- src/main/java/com/vamsi/MoneyManagerApp/service/JwtService.java
+++ src/main/java/com/vamsi/MoneyManagerApp/service/JwtService.java
@@ -47,7 +47,7 @@ public class JwtService {
                 .setClaims(claims)
                 .setSubject(email)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis()+1000*60*3))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*60*10))
                 .signWith(getKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
